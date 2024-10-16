6411001ae4088b6b0148107f43484522de71d5db
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:16:20 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,10 @@
 -- a trigger that after an update checks if the email is valid or not
+
 CREATE TRIGGER  validate_email 
 AFTER UPDATE
 FOR EACH ROW
+BEGIN
 IF OLD.email <> NEW.email THEN
   SET NEW.valid_email = 0
 END IF;
+END
