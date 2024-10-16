698b5e7f9075bc49d957c888a2ef47c928ec0408
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:19:01 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,8 @@
 -- a trigger that after an update resets arre valid_email
 -- only when the email has been changed
 DELIMITER $$ 
-CREATE TRIGGER  validate_email 
-AFTER UPDATE
+CREATE TRIGGER validate_email 
+AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
 IF OLD.email <> NEW.email THEN
