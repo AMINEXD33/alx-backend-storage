1bf0928e4aca11e79e5ca5e5424914a0b3c822df
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:36:38 +0100

    task 5

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+--a stored procedure AddBonus that adds a new correction for a student
+DELIMITER ##
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255),  IN score FLOAT)
+BEGIN
+  DECLARE pr_id INT;
+  SELECT id INTO pr_id FROM projects
+  WHERE name = project_name;
+  IF pr_id IS NULL THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    SELECT LAST_INSERT_ID() INTO pr_id;
+  END IF;
+  INSERT INTO corrections VALUES (user_id, project_id, score);
+END
+##
+DELIMITER;
