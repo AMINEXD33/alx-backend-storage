0d73c0ddba6c0e1fd99e202478739fe1562ee43e
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:39:39 +0100

    task 5

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,13 +3,16 @@ DELIMITER ##
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255),  IN score FLOAT)
 BEGIN
   DECLARE pr_id INT;
-  SELECT id INTO pr_id FROM projects
+
+  SELECT id INTO pr_id 
+  FROM projects
   WHERE name = project_name;
+
   IF pr_id IS NULL THEN
     INSERT INTO projects (name) VALUES (project_name);
     SELECT LAST_INSERT_ID() INTO pr_id;
   END IF;
-  INSERT INTO corrections VALUES (user_id, project_id, score);
+  INSERT INTO corrections VALUES (user_id, pr_id, score);
 END
 ##
 DELIMITER;
