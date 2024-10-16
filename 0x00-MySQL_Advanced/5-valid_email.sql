4775ae310a046d053d41ef34e57e19ff91e7edc8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:19:47 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,9 @@ CREATE TRIGGER validate_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF OLD.email <> NEW.email THEN
-  SET NEW.valid_email = 0
+IF OLD.email <> NEW.email 
+THEN
+  SET NEW.valid_email = 0;
 END IF;
 END
 $$
