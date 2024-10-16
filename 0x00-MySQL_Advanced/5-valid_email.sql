eace2f1e7ce0e3b263e732ef93c3e12f4b8e7b60
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 23:17:45 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
--- a trigger that after an update checks if the email is valid or not
-
+-- a trigger that after an update resets arre valid_email
+-- only when the email has been changed
+DELIMITER $$ 
 CREATE TRIGGER  validate_email 
 AFTER UPDATE
 FOR EACH ROW
@@ -8,3 +9,5 @@ IF OLD.email <> NEW.email THEN
   SET NEW.valid_email = 0
 END IF;
 END
+$$
+DELIMITER;
