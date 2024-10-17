96eaadb006df05600868b1839533736db62849c8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-17 23:45:03 +0100

    task 6

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- computes and store the average score for a student
+
+DELIMITER ##
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE avg FLOAT;
+  SELECT AVG(score) INTO avg FROM corrections as X WHERE X.user_id = user_id;
+  UPDATE SET average_score = avg WHERE user_id = user_id;
+END
+##
+DELIMITER;
+  
