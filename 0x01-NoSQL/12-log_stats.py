a6c3a00bcdd0a98f2e009072336b081a703daf39
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-22 23:14:45 +0100

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""  Operations with Python using pymongo """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """this script is trying to get data of some logs"""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+
+    docs_count = nginx_collection.count_documents({})
+    print(f"{docs_count} logs")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    get_and_path_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{get_and_path_count} status check")
