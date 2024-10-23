4cb423ed1cbbef6558c52eedcc754b1d01ed0a7b
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-23 20:26:02 +0100

    task 9

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" A caching request module
+"""
+import redis
+import requests
+from functools import wraps
+from typing  import Callable, Any
+
+
+def track_get_page(fn: Callable) -> Callable:
+    """ getter for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """
+            wrapper that checks if a url's data is cached
+            and tracks how many times the get_page is called.
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
+
+
+@track_get_page
+def get_page(url: str) -> str:
+    """
+    Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
