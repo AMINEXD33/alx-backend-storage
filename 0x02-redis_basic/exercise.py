64cc7f2dbc3ef5fecf1a029940d3c20111e999da
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-23 20:23:12 +0100

    task 9

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -48,16 +48,16 @@ def replay(fn: Callable) -> None:
     client = redis.Redis()
     calls = client.get(fn.__qualname__).decode("utf-8")
     inputs = [
-        inpu.decode("utf-8")
-        for inpu in client.lrange(f"{fn.__qualname__}:inputs", 0, -1)
+        input.decode("utf-8")
+        for input in client.lrange(f"{fn.__qualname__}:inputs", 0, -1)
     ]
     outputs = [
         output.decode("utf-8")
         for output in client.lrange(f"{fn.__qualname__}:outputs", 0, -1)
     ]
     print(f"{fn.__qualname__} was called {calls} times:")
-    for inpu, output in zip(inputs, outputs):
-        print(f"{fn.__qualname__}(*{inpu}) -> {output}")
+    for input, output in zip(inputs, outputs):
+        print(f"{fn.__qualname__}(*{input}) -> {output}")
 
 
 class Cache:
